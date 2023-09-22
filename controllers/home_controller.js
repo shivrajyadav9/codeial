20573da994ae2466cd11f3b2f2bf43ddc3f281cf
Shivraj <rajs777yadav@gmail.com>
2023-09-22 16:10:40 +0530

    add home and user profile views and partials in them

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,7 @@
 //module.exports.actionName=function(req,res){};
 module.exports.home = function (req, res) {
-    return res.end('<h1>Express is up for codeial!');
+    // return res.end('<h1>Express is up for codeial!');
+    return res.render('home', {
+        title: 'Codeial Home'
+    });
 }
--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+//use express router
 app.use('/', require('./routes/index'));
+
+//set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function (err) {
     if (err) {
