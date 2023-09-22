d459a7fcf33db9957a344fec3f3a67ec0f422d44
Shivraj <rajs777yadav@gmail.com>
2023-09-22 21:10:31 +0530

    get sign up data and handle sign up request

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function (req, res) {
     // return res.end('<h1>User Profile</h1>');
 
@@ -19,8 +21,26 @@ module.exports.signUp = function (req, res) {
 }
 //get the sign up data
 module.exports.create = function (req, res) {
-    //TODO later
+    if (req.body.password != req.body.confirm_password) {
+        console.log('password != confirm password');
+        return res.redirect('back');
+    }
+    User.findOne({ email: req.body.email })
+        .catch((err) => { console.log('Error in finding user in singing up'); return; })
+        .then((user) => {
+            if (!user) {
+                User.create(req.body)
+                    .catch((err) => { console.log('Error in creating user in signing up'); return; })
+                    .then((user) => {
+                        return res.redirect('/users/sign-in');
+                    })
+            } else {
+                return res.redirect('back');
+            }
+
+        });
 }
+
 //sign in and create a session for the user
 module.exports.createSession = function (req, res) {
     //TODO later
