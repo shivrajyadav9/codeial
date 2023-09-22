eb52542c8de97a9d3b90c754a56d652cd9978301
Shivraj <rajs777yadav@gmail.com>
2023-09-22 16:21:51 +0530

    add express ejs layouts

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+//use express layouts
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+
 //use express router
 app.use('/', require('./routes/index'));
 
