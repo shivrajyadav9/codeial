f3ca9f31a75f98579320568b2d10066a73649aff
Shivraj <rajs777yadav@gmail.com>
2023-10-03 15:34:56 +0530

    set up passport local stratgey

---
+++ config/passport-local-stratgey.js
@@ -0,0 +1,47 @@
+const passport = require('passport');
+
+const LocalStratgey = require('passport-local').Strategy;
+
+const User = require('../models/user');
+
+//authentication using passport
+passport.use(new LocalStratgey({
+    usernameField: 'email'
+},
+    function (email, password, done) {
+        //done is a function inbuit in passport takes two arguments
+        //find the user and establish the identity
+        User.findOne({ email: email }, function (err, user) {
+            if (err) {
+                console.log('error in finding user --> passport');
+                return done(err);
+            }
+            if (!user || user.password != password) {
+                console.log('invalid username/password');
+                return done(null, false);
+            }
+            return done(null, user);
+        })
+
+    }
+));
+
+//serialize the user to decide which key is to be kept in the cookies
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+})
+
+//deserializing the user from the key in the cookies
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        if (err) {
+            console.log('error in finding user --> passport');
+            return done(err);
+        }
+        return done(null, user);
+    });
+});
+
+module.exports=passport;
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,6 @@
 //module.exports.actionName=function(req,res){};
 module.exports.home = function (req, res) {
     // return res.end('<h1>Express is up for codeial!');
-    res.cookie('user_id', '25');
-    console.log(req.cookies);
-
     return res.render('home', {
         title: 'Codeial Home'
     });
