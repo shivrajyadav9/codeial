fe923735366a5298beae73c7109b86c96e8eb5b1
Shivraj <rajs777yadav@gmail.com>
2023-10-05 16:39:26 +0530

    populate comments and display them

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,6 +3,12 @@ const Post = require('../models/post');
 module.exports.home = function (req, res) {
     // return res.end('<h1>Express is up for codeial!');
     Post.find({}).populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
         .catch((err) => {
             console.log('error in finding posts');
         })
