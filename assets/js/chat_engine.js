5f6cc66a67bcb89cb636401be107bba40eaa6d05
Shivraj <rajs777yadav@gmail.com>
2023-10-24 15:56:31 +0530

    commit

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -4,7 +4,7 @@ class ChatEngine {
         this.userEmail = userEmail;
         this.userName=userName;
 
-        this.socket = io.connect('http://localhost:5000');
+        this.socket = io.connect('http://0.0.0.0:5000');
 
         if (this.userEmail) {
             this.connectionHandler();
